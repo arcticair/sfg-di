9a857fb4c21ec000fab3173a653fd46d49da46d9
serzhi <servaszhi@gmail.com>
2020-02-05 02:02:49 +0300

    controller added

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,26 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
+		ConstructorInjectedController cic = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(cic.getGreeting());
+
+		PropertyInjectedController pic = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(pic.getGreeting());
+
 	}
 
 }
